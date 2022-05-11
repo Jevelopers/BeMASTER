1bc85b8d8d075e3dbc563995842320a18ad6ff16
Jevelopers <jwchavarro@corhuila.edu.co>
2022-05-10 20:18:22 -0500

    Configuracion - informacion del proyecto

--- rickmarty/src/index.js
+++ rickmarty/src/index.js
@@ -1,3 +1,12 @@
+/* 
+    ------------------------------------------
+    Nombre del proyecto: BeMaster - Prueba ingreso
+    Desarrollador: John Wilmer Chavarro Urrea
+    Version: 0.1.1
+    Fecha inicio: 10/05/2022
+    ------------------------------------------
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
